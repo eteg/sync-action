519b909d7dda75cd6412ff5e8c99efbf636bd899
augustobritodev <augustobritodev@icloud.com>
2022-09-01 08:41:26 -0300

    v1

--- dist/index.js
+++ dist/index.js
@@ -24354,8 +24354,12 @@ async function init() {
 
   shell.exec(`mkdir -p ~/.ssh`)
   shell.exec(`echo ${SRC_SSH} > ~/.ssh/id_rsa`)
-  shell.exec(`chmod 600 ~/.ssh/id_rsa`)
+  shell.exec(`chmod 700 ~/.ssh/id_rsa`)
 
+  shell.echo(`git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"`)
+  // TODO Add hosts
+  //shell.exec(`git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=~/.ssh/known_hosts"`)
+  
   shell.exec(`git remote add mirror git@bitbucket.org:${USER}/${repository.name}.git`)
   shell.exec(`git push --tags --force --prune mirror refs/remotes/origin/*:refs/heads/*`)
 }
